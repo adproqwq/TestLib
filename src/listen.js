76675fd685f5bc6e10713660f9bc6f04bcdd5d8f
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-09-24 13:08:00 +0800

    持续监听

--- src/listen.js
+++ src/listen.js
@@ -1,3 +1,11 @@
+document.getElementById("choose").addEventListener("change",function(listen){
+    if(a.target.tagName == "SELECT"){
+        var userSelect = document.getElementById("choose");
+        var index = userSelect.selectedIndex;
+        if(userSelect.options[index].value == "yylj") yylj();
+        else if(userSelect.options[index].value == "cs") cs();
+    };
+});
 function getArti(){
     var userSelect = document.getElementById("choose");
     var index = userSelect.selectedIndex;
