a4e78a2ef17e7686f9f230f30ee43b9dc47b8a79
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-09-24 13:01:19 +0800

    增加新词库

--- src/listen.js
+++ src/listen.js
@@ -2,4 +2,5 @@ function getArti(){
     var userSelect = document.getElementById("choose");
     var index = userSelect.selectedIndex;
     if(userSelect.options[index].value == "yylj") yylj();
+    else if(userSelect.options[index].value == "cs") cs();
 };
