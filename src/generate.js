5f3da5a4128f923c4e9e81c23d03bf5b7960e6e9
adproqwq <101449500+adproqwq@users.noreply.github.com>
2023-10-27 22:25:49 +0800

    精简生成题库后跳转的链接

--- src/generate.js
+++ src/generate.js
@@ -17,11 +17,9 @@ function generate(i){
     }
     getQ = getQ.substring(0,getQ.length - 1);
     getA = getA.substring(0,getA.length - 1);
-    //拼接
-    json = `var title = "${getTitle}";var author = "${getAuthor}";var version = "${getVersion}";var description = "${getDiscription}";var Q = [];var A = [];function redefine_${getFileName}(){Q = [${getQ}];A = [${getA}];return ${getFileName}();};var getQue, ranPos;var cho = [];var backup = [];function ${getFileName}(){getQue = Math.floor(Math.random()*Q.length);cho = [];backup = [...A];for(var i = 0;i < 4;i++){var getRanNum = Math.floor(Math.random()*backup.length);cho[i] = backup[getRanNum];backup.splice(getRanNum,1);}if(cho.lastIndexOf(A[getQue]) == -1){ranPos = Math.floor(Math.random()*4);cho[ranPos] = A[getQue];}else ranPos = cho.lastIndexOf(A[getQue]);if(version == "") document.getElementById("lib").innerHTML = "当前题库版本：1.0.0";else document.getElementById("lib").innerHTML = "当前题库版本：" + version;document.getElementById("q").innerHTML = Q[getQue];document.getElementById("c0").textContent = cho[0];document.getElementById("c1").textContent = cho[1];document.getElementById("c2").textContent = cho[2];document.getElementById("c3").textContent = cho[3];return ranPos;};function ${getFileName}_judge(button,ranPos){if(button == ranPos){alert("${getCorrectAlert}");return "correct";}else alert("${getWrongAlert}");};`;
 };
 function test(i){
-    window.open(encodeURI('./index.html?' + 'json=' + json + '?title=' + getTitle + '?author=' + getAuthor + '?version=' + getVersion + '?desc=' + getDiscription + '?fileName=' + getFileName + '?c=' + getCorrectAlert + '?w=' + getWrongAlert + '?q=' + getQ + '?a=' + getA + '?i=' + String(i))); 
+    window.open(encodeURI('./index.html?' + '?title=' + getTitle + '?author=' + getAuthor + '?version=' + getVersion + '?desc=' + getDiscription + '?fileName=' + getFileName + '?c=' + getCorrectAlert + '?w=' + getWrongAlert + '?q=' + getQ + '?a=' + getA + '?i=' + String(i))); 
 };
 function save(){
     console.log(json);
