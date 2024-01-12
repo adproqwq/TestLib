25ed0c26bf37c3897b35fbfcc37d993b0c19d0e4
adproqwq <adpro_qwq@outlook.com>
2024-01-12 23:20:14 +0800

    feat: 重构题库编辑器

--- src/generate.js
+++ src/generate.js
@@ -1,27 +1,90 @@
-var getTitle, getAuthor, getVersion, getDiscription, getFileName, getCorrectAlert, getWrongAlert, getQ, getA;
-function generate(i){
-    //获取信息
-    getTitle = document.getElementById("title").value;
-    getAuthor = document.getElementById("author").value;
-    getVersion = document.getElementById("version").value;
-    getDiscription = document.getElementById("description").value;
-    getFileName = document.getElementById("fileName").value;
-    getCorrectAlert = document.getElementById("correct").value;
-    getWrongAlert = document.getElementById("wrong").value;
-    getQ = "'";
-    getA = "'";
-    for(var j = 0;j < i;j++){
-        getQ = getQ + document.getElementById("Q" + String(j + 1)).value + "','";
-        getA = getA + document.getElementById("A" + String(j + 1)).value + "','";
+function generate() {
+  //获取信息
+  let getName = document.getElementById("name").value;
+  let getAuthor = document.getElementById("author").value;
+  let getVersionName = document.getElementById("versionName").value;
+  let getVersionCode = document.getElementById("versionCode").value;
+  let getDesc = document.getElementById("desc").value;
+  let getLibName = document.getElementById("libName").value;
+  let getCorrectAlert = document.getElementById("correct").value;
+  let getWrongAlert = document.getElementById("wrong").value;
+  let getData = document.getElementById('data').value;
+  let data = getData.split(/[(\r\n)\r\n]+/);
+  data.forEach((item, index) => {
+    if (!item) data.splice(index, 1);
+  });
+  let QA = ``;
+  data.forEach((item) => {
+    let eachData = item.split(',');
+    let length = eachData.length;
+    let choice = ``;
+    for(let i = 2; i < length; i++){
+      choice += `'${eachData[i]}',`;
     }
-    getQ = getQ.substring(0,getQ.length - 1);
-    getA = getA.substring(0,getA.length - 1);
+    QA += `
+      {
+        Q: '${eachData[0]}',
+        A: '${eachData[1]}',
+        choice: [${choice}],
+      },
+    `;
+  });
+  let libTemplate = `
+    {
+      name: '${getName}',
+      libName: '${getLibName}',
+      desc: '${getDesc}',
+      author: '${getAuthor}',
+      versionName: '${getVersionName}',
+      versionCode: ${getVersionCode},
+      prompt: {
+        correct: '${getCorrectAlert}',
+        wrong: '${getWrongAlert}',
+      },
+      data: [
+        ${QA}
+      ],
+    }
+  `;
+  let blob = new Blob([libTemplate],{
+    type: 'application/json'
+  });
+  const downloadURL = URL.createObjectURL(blob);
+  const aTag = document.createElement('a');
+  aTag.href = downloadURL;
+  aTag.download = `${getLibName}.json5`;
+  aTag.click();
+  URL.revokeObjectURL(downloadURL);
 };
-function test(i){
-    window.open(encodeURI('./index.html?' + '?title=' + getTitle + '?author=' + getAuthor + '?version=' + getVersion + '?desc=' + getDiscription + '?fileName=' + getFileName + '?c=' + getCorrectAlert + '?w=' + getWrongAlert + '?q=' + getQ + '?a=' + getA + '?i=' + String(i))); 
-};
-function save(){
-    navigator.clipboard.writeText('https://testlib.adproqwq.xyz/index.html?title=' + getTitle + '?author=' + getAuthor + '?version=' + getVersion + '?desc=' + getDiscription + '?fileName=' + getFileName + '?c=' + getCorrectAlert + '?w=' + getWrongAlert + '?q=' + getQ + '?a=' + getA + '?i=' + String(i)).then(() => {
-        alert('已复制到剪切板，分享题库时发送该链接即可。');
-    });
+
+function readLib(){
+  const objFile = document.getElementById('upload');
+  if(objFile.value === ''){
+      alert('请选择题库文件');
+      return
+  }
+  const libFile = objFile.files;
+  const reader = new FileReader();
+  reader.readAsText(libFile[0],'UTF-8');
+  reader.onload = function(e){
+    let data = e.target.result;
+    data = JSON5.parse(data);
+    document.getElementById("name").value = data.name;
+    document.getElementById("author").value = data.author;
+    document.getElementById("versionName").value = data.versionName;
+    document.getElementById("versionCode").value = data.versionCode;
+    document.getElementById("desc").value = data.desc;
+    document.getElementById("libName").value = data.libName;
+    document.getElementById("correct").value = data.prompt.correct;
+    document.getElementById("wrong").value = data.prompt.wrong;
+    let QA = ``;
+    for(let i in data.data){
+      let choice = ``;
+      for(let j in data.data[i].choice){
+        choice += `,${data.data[i].choice[j]}`
+      }
+      QA += `${data.data[i].Q},${data.data[i].A}${choice}\r`;
+    }
+    document.getElementById('data').value = QA;
+  };
 };
